feb6825e8a4101fd4b8a3bf4713ccce0109a67b1
ingue <dlsrb8949@gmail.com>
2019-06-03 19:36:02 +0900

    Apply Transaction like router

--- routes/webtoon/like.js
+++ routes/webtoon/like.js
@@ -7,29 +7,38 @@ const statusCode = require('../../module/utils/statusCode');
 const resMessage = require('../../module/utils/responseMessage');
 const db = require('../../module/pool');
 
-router.post('/', async (req, res) => {//insert 쿼리 두개 나중에 트랜잭션으로 바꾸기 
-
+router.post('/', async (req, res) => {
 
     const insertLikeListQuery =
         'INSERT INTO likelist (user_idx,webtoon_idx) VALUES (?,?)';
-    const insertLikeListResult = await db.queryParam_Parse(insertLikeListQuery,
-        [req.body.user_idx, req.body.webtoon_idx])
+    const updateLikeQuery = 'UPDATE webtoon SET webtoon_like = webtoon_like+1 WHERE webtoon_idx = ?';
 
-    if (!insertLikeListResult) {
+    const insertTransaction = await db.Transaction(async (connection) => {
+        const insertLikeListResult = await connection.query(insertLikeListQuery,
+            [req.body.user_idx, req.body.webtoon_idx])
+        const updateLikeResult = await connection.query(updateLikeQuery, [req.body.webtoon_idx]);
+    });
+    if (!insertTransaction) {
         res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.INSERT_LIKE_FAILED));
     } else { //쿼리문이 성공했을 때
         res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.INSERT_LIKE_SUCCESS));
     }
 
 });
+
+
+
 router.delete('/', async (req, res) => {
 
     const deleteLikeListQuery =
         'DELETE FROM likelist WHERE user_idx=? AND webtoon_idx=?';
-    const deleteLikeListResult = await db.queryParam_Parse(deleteLikeListQuery,
-        [req.body.user_idx, req.body.webtoon_idx])
-
-    if (!deleteLikeListResult) {
+    const updateLikeQuery = 'UPDATE webtoon SET webtoon_like = webtoon_like-1 WHERE webtoon_idx = ?';
+    const deleteTransaction = await db.Transaction(async (connection) => {
+        const deleteLikeListResult = await db.queryParam_Parse(deleteLikeListQuery,
+            [req.body.user_idx, req.body.webtoon_idx])
+        const updateLikeResult = await connection.query(updateLikeQuery, [req.body.webtoon_idx]);
+    });
+    if (!deleteTransaction) {
         res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.DELETE_LIKE_FAILED));
     } else { //쿼리문이 성공했을 때
         res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.DELETE_LIKE_SUCCESS));
